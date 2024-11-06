e182a827230624269738e74e8f2d565ad5110a25
Lya <lasvmateo@gmail.com>
2024-11-06 11:31:17 +0100

    ,

--- menu/pages.js
+++ menu/pages.js
@@ -19,10 +19,3 @@ function openPage(evt, cityName) {
     evt.currentTarget.className += " active";
   }
 
-  const menuToggle = document.querySelector('.toggle');
-const showcase = document.querySelector('.showcase');
-
-menuToggle.addEventListener('click', () => {
-  menuToggle.classList.toggle('active');
-  showcase.classList.toggle('active');
-})
